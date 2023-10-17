93e628d17ebd83577b91765a9cf4cc1405d9d241
Rasit Evduzen <r.evduzen@hotmail.com>
2023-10-17 23:33:36 +0300

    $$

---
+++ Endianles/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+/*
+little endian
+    higher memory
+       ----->
+ +----+----+----+----+
+ |0x01|0x00|0x00|0x00|
+ +----+----+----+----+
+ ^
+ |
+&data
+
+big endian
++----+----+----+----+
+ |0x00|0x00|0x00|0x01|
+ +----+----+----+----+
+ ^
+ |
+&data
+*/
+
+int main()
+{
+   unsigned int i = 1;          // 00000000 00000000 00000000 00000001 -> 0x 00 00 00 01      "4Byte Unsigned Int"
+   char *c = (char*)&i;         // Int val to Char val    "1Byte"
+   printf(*c ? "Little endian\n":"Big endian\n");
+   return 0;
+}
