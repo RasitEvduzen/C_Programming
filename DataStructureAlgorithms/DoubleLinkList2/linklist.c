371adb1b7d61c5acaa39526ff8762cee289919dd
Rasit Evduzen <r.evduzen@hotmail.com>
2024-08-20 00:02:14 +0300

    update!

--- DataStructureAlgorithms/DoubleLinkList2/linklist.c
+++ DataStructureAlgorithms/DoubleLinkList2/linklist.c
@@ -4,7 +4,7 @@
 
 
 /* Static Functions Prototypes */
-static bool disp(NODE *node);
+static bool disp(DATATYPE *val);
 
 /* Function Definitions */
 HLINKLIST create_link_list(void)
@@ -141,31 +141,31 @@ void destroy_link_list(HLINKLIST hlinklist)
     free(hlinklist);
 }
 
-bool walk_link_list(HLINKLIST hlinklist, bool(*proc)(NODE *))
+bool walk_link_list(HLINKLIST hlinklist, bool(*proc)(DATATYPE *))
 {
     if(proc == NULL)
         proc = &disp;
     for (NODE *node = hlinklist->head.next; node != &hlinklist->head; node = node->next)
-        if(!proc(node))
+        if(!proc(&node->val))
             return false;
     putchar('\n');
     return true;
 }
 
-bool walk_link_list_rev(HLINKLIST hlinklist, bool(*proc)(NODE *))
+bool walk_link_list_rev(HLINKLIST hlinklist, bool(*proc)(DATATYPE *))
 {
     if(proc == NULL)
         proc = &disp;
     for (NODE *node = hlinklist->head.prev; node != &hlinklist->head; node = node->prev)
-        if(!proc(node))
+        if(!proc(&node->val))
             return false;
     putchar('\n');
     return true;
 }
 
-static bool disp(NODE *node)
+static bool disp(DATATYPE *val)
 {
-    printf("%d ", node->val);
+    printf("%d ", *val);
     fflush(stdout);
     return true;
 }
